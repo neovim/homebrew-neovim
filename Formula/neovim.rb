88552de90f82b1d54e2d8199d5bc7112fe81c986
Florian Walch <florian@fwalch.com>
2015-07-22 11:40:48 +0300

    Formula: Update caveats message.
    Adapted from the post-install message of the Arch Linux PKGBUILD.

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -75,11 +75,21 @@ class Neovim < Formula
   end
 
   def caveats; <<-EOS.undent
+      The Neovim executable is called 'nvim'. To use your existing Vim
+      configuration:
+          ln -s ~/.vimrc ~/.nvimrc
+          ln -s ~/.vim ~/.nvim
+      See ':help nvim' for more information on Neovim.
+
+      When upgrading Neovim, check the following page for breaking changes:
+          https://github.com/neovim/neovim/wiki/Following-HEAD
+
       If you want support for Python plugins such as YouCompleteMe, you need
       to install a Python module in addition to Neovim itself.
 
-      Execute `:help nvim-python` in nvim or see
-      http://neovim.io/doc/user/nvim_python.html for more information.
+      Execute ':help nvim-python' in nvim or see the following page for more
+      information:
+          http://neovim.io/doc/user/nvim_python.html
     EOS
   end
 
