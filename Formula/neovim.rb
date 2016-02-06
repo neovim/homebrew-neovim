97444e721b6706631dd18f759e66d3aec9fd46bc
David Holm <dholmster@gmail.com>
2016-02-06 17:21:16 +0100

    deps: Install dependencies from Homebrew
    Install Neovim dependencies from Homebrew rather than bundling them with
    the formula.

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -1,96 +1,11 @@
 class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io"
-
-  stable do
-    url "https://github.com/neovim/neovim/archive/v0.1.1.tar.gz"
-    sha256 "f39bcab23457c66ce0d67dcf8029743703f860413db0070f75d4f0ffad27c6c1"
-
-    # Third-party dependencies for latest release.
-    resource "libuv" do
-      url "https://github.com/libuv/libuv/archive/v1.7.3.tar.gz"
-      sha256 "db5d46318e18330c696d954747036e1be8e2346411d4f30236d7e2f499f0cfab"
-    end
-
-    resource "msgpack" do
-      url "https://github.com/msgpack/msgpack-c/archive/cpp-1.0.0.tar.gz"
-      sha256 "afda64ca445203bb7092372b822bae8b2539fdcebbfc3f753f393628c2bcfe7d"
-    end
-
-    resource "luajit" do
-      url "http://luajit.org/download/LuaJIT-2.0.4.tar.gz"
-      sha256 "620fa4eb12375021bef6e4f237cbd2dd5d49e56beb414bee052c746beef1807d"
-    end
-
-    resource "luarocks" do
-      url "https://github.com/keplerproject/luarocks/archive/5d8a16526573b36d5b22aa74866120c998466697.tar.gz"
-      sha256 "cae709111c5701235770047dfd7169f66b82ae1c7b9b79207f9df0afb722bfd9"
-    end
-
-    resource "unibilium" do
-      url "https://github.com/mauke/unibilium/archive/v1.2.0.tar.gz"
-      sha256 "623af1099515e673abfd3cae5f2fa808a09ca55dda1c65a7b5c9424eb304ead8"
-    end
-
-    resource "libtermkey" do
-      url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.18.tar.gz"
-      sha256 "239746de41c845af52bb3c14055558f743292dd6c24ac26c2d6567a5a6093926"
-    end
-
-    resource "libvterm" do
-      url "https://github.com/neovim/libvterm/archive/1b745d29d45623aa8d22a7b9288c7b0e331c7088.tar.gz"
-      sha256 "3fc75908256c0d158d6c2a32d39f34e86bfd26364f5404b7d9c03bb70cdc3611"
-    end
-
-    resource "jemalloc" do
-      url "https://github.com/jemalloc/jemalloc/releases/download/4.0.2/jemalloc-4.0.2.tar.bz2"
-      sha256 "0d8a9c8a98adb6983e0ccb521d45d9db1656ef3e71d0b14fb333f2c8138f4611"
-    end
-  end
+  url "https://github.com/neovim/neovim/archive/v0.1.1.tar.gz"
+  sha256 "f39bcab23457c66ce0d67dcf8029743703f860413db0070f75d4f0ffad27c6c1"
 
   head do
     url "https://github.com/neovim/neovim.git"
-
-    # Third-party dependencies for latest repo revision.
-    resource "libuv" do
-      url "https://github.com/libuv/libuv/archive/v1.7.3.tar.gz"
-      sha256 "db5d46318e18330c696d954747036e1be8e2346411d4f30236d7e2f499f0cfab"
-    end
-
-    resource "msgpack" do
-      url "https://github.com/msgpack/msgpack-c/archive/cpp-1.0.0.tar.gz"
-      sha256 "afda64ca445203bb7092372b822bae8b2539fdcebbfc3f753f393628c2bcfe7d"
-    end
-
-    resource "luajit" do
-      url "http://luajit.org/download/LuaJIT-2.0.4.tar.gz"
-      sha256 "620fa4eb12375021bef6e4f237cbd2dd5d49e56beb414bee052c746beef1807d"
-    end
-
-    resource "luarocks" do
-      url "https://github.com/keplerproject/luarocks/archive/5d8a16526573b36d5b22aa74866120c998466697.tar.gz"
-      sha256 "cae709111c5701235770047dfd7169f66b82ae1c7b9b79207f9df0afb722bfd9"
-    end
-
-    resource "unibilium" do
-      url "https://github.com/mauke/unibilium/archive/v1.2.0.tar.gz"
-      sha256 "623af1099515e673abfd3cae5f2fa808a09ca55dda1c65a7b5c9424eb304ead8"
-    end
-
-    resource "libtermkey" do
-      url "http://www.leonerd.org.uk/code/libtermkey/libtermkey-0.18.tar.gz"
-      sha256 "239746de41c845af52bb3c14055558f743292dd6c24ac26c2d6567a5a6093926"
-    end
-
-    resource "libvterm" do
-      url "https://github.com/neovim/libvterm/archive/1b745d29d45623aa8d22a7b9288c7b0e331c7088.tar.gz"
-      sha256 "3fc75908256c0d158d6c2a32d39f34e86bfd26364f5404b7d9c03bb70cdc3611"
-    end
-
-    resource "jemalloc" do
-      url "https://github.com/jemalloc/jemalloc/releases/download/4.0.2/jemalloc-4.0.2.tar.bz2"
-      sha256 "0d8a9c8a98adb6983e0ccb521d45d9db1656ef3e71d0b14fb333f2c8138f4611"
-    end
   end
 
   depends_on "cmake" => :build
@@ -99,28 +14,28 @@ class Neovim < Formula
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext" => :build
+
+  depends_on "homebrew/headonly/libvterm"
+  depends_on "jemalloc"
+  depends_on "libtermkey"
+  depends_on "libuv"
+  depends_on "luajit"
+  depends_on "luarocks"
+  depends_on "msgpack"
+  depends_on "unibilium"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
+
+  depends_on "lpeg" => :lua
+  depends_on "lua-messagepack" => :lua
+  depends_on "luabitop" => :lua
 
   def install
     ENV.deparallelize
     ENV["HOME"] = buildpath
 
-    resources.each do |r|
-      r.stage(buildpath/"deps-build/build/src/#{r.name}")
-    end
-
-    cd "deps-build" do
-      ohai "Building third-party dependencies."
-      system "cmake", "../third-party", "-DUSE_BUNDLED_BUSTED=OFF",
-             "-DUSE_EXISTING_SRC_DIR=ON", *std_cmake_args
-      system "make", "VERBOSE=1"
-    end
-
     mkdir "build" do
-      ohai "Building Neovim."
       build_type = build.head? ? "Dev" : "RelWithDebInfo"
-      cmake_args = std_cmake_args + ["-DDEPS_PREFIX=../deps-build/usr",
-                                     "-DCMAKE_BUILD_TYPE=#{build_type}"]
+      cmake_args = std_cmake_args + ["-DCMAKE_BUILD_TYPE=#{build_type}"]
       if OS.mac?
         cmake_args += ["-DIconv_INCLUDE_DIRS:PATH=/usr/include",
                        "-DIconv_LIBRARIES:PATH=/usr/lib/libiconv.dylib"]
