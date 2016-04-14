acd622330234b773f860d4e5aad8ee4e63d63fb3
Florian Walch <florian@fwalch.com>
2016-04-14 21:03:15 +0200

    Disable building Luv. #145
    Luv is only required for the Lua client (i.e. for running the tests), so
    we can disable it.
    
    Resolves #143.

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -114,7 +114,8 @@ class Neovim < Formula
     cd "deps-build" do
       ohai "Building third-party dependencies."
       system "cmake", "../third-party", "-DUSE_BUNDLED_BUSTED=OFF",
-             "-DUSE_EXISTING_SRC_DIR=ON", *std_cmake_args
+             "-DUSE_BUNDLED_LUV=OFF", "-DUSE_EXISTING_SRC_DIR=ON",
+             *std_cmake_args
       system "make", "VERBOSE=1"
     end
 
