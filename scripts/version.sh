bde7b971e92a723c99e76b4a83091d18fd5a4fef
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-10-04 21:27:19 -0300

    Created function to find current version inside CMakeLists.txt

--- scripts/version.sh
+++ scripts/version.sh
@@ -17,8 +17,8 @@ if [ -d "$TMP_DIR/neovim" ]; then
     echo "Found temporary neovim repository.";
     pushd "$TMP_DIR/neovim";
         echo "Updating repository with remote"
-        git pull origin master;
-        git remote update;
+       # git pull origin master;
+       # git remote update;
     popd;
 else
     echo "Cloning neovim repository to temporary directory:"
@@ -26,3 +26,24 @@ else
         git clone $NEOVIM_URI;
     popd;
 fi;
+
+
+function find_version ()
+{
+
+    if [ ! -f "CMakeLists.txt" ]; then
+        echo "CMakeLists.txt not found. Aborting.";
+        exit 1;
+    fi;
+
+    echo $(grep "set($1" CMakeLists.txt | cut -d " " -f 2 | tr -d ")");
+}
+
+echo "Finding nvim current version..."
+pushd "$TMP_DIR/neovim";
+    MAJOR=$(find_version NVIM_VERSION_MAJOR);
+    MINOR=$(find_version NVIM_VERSION_MINOR);
+    PATCH=$(find_version NVIM_VERSION_PATCH);
+popd
+
+echo "Current version: $MAJOR.$MINOR.$PATCH";
