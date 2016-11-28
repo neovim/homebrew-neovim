bc357f4b0a8c33ea91499a9cd6776c9ddbf33685
Justin M. Keyes <justinkz@gmail.com>
2016-11-28 14:45:27 +0100

    Disable jemalloc (#200)
    Workaround / Closes neovim/neovim#5681
    
    Reproduced by mhinz building from source (without homebrew), with this patch:
    
    diff --git i/third-party/cmake/BuildJeMalloc.cmake w/third-party/cmake/BuildJeMalloc.cmake
    index 637aadaad..6efbff9aa 100644
    --- i/third-party/cmake/BuildJeMalloc.cmake
    +++ w/third-party/cmake/BuildJeMalloc.cmake
    @@ -20 +20 @@ ExternalProject_Add(jemalloc
    -     CC=${DEPS_C_COMPILER} --prefix=${DEPS_INSTALL_DIR}
    +     CC=${DEPS_C_COMPILER} --prefix=${DEPS_INSTALL_DIR} --with-jemalloc-prefix=
    @@ -22 +22 @@ ExternalProject_Add(jemalloc
    -  INSTALL_COMMAND ${MAKE_PRG} install_include install_lib_static)
    +  INSTALL_COMMAND ${MAKE_PRG} install_include)
    
    References:
    https://github.com/DomT4/homebrew-core/commit/300ff53e8e10f582846c7e898ea602dc2820fe3a
    https://github.com/neovim/neovim/commit/8130eb1191aece52d8b2790302abf1bd09aaf90f

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -13,7 +13,6 @@ class Neovim < Formula
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
-  depends_on "jemalloc" => :recommended
   depends_on "libuv"
   depends_on "msgpack"
   depends_on "unibilium"
@@ -66,7 +65,9 @@ class Neovim < Formula
       cmake_args = std_cmake_args + ["-DDEPS_PREFIX=../deps-build/usr",
                                      "-DCMAKE_BUILD_TYPE=#{build_type}"]
 
-      cmake_args += ["-DENABLE_JEMALLOC=OFF"] if build.without?("jemalloc")
+      # Disable jemalloc, see https://github.com/neovim/neovim/issues/5681
+      # cmake_args += ["-DENABLE_JEMALLOC=OFF"] if build.without?("jemalloc")
+      cmake_args += ["-DENABLE_JEMALLOC=OFF"]
 
       if OS.mac?
         unless build.head?
